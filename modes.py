9a2c929d7b327681210ceb812908cef2c8d8199d
Roman Chefranov <roman_chefr@bk.ru>
2018-10-29 18:37:12 +0300

    Test mode first realization (#2)
    * Test mode first realization

--- modes.py
+++ modes.py
@@ -62,7 +62,7 @@ class Mode(abc.ABC):
         """ """
         
     @abc.abstractmethod
-    def _parse_resource(self):
+    def _parse_resource(self, resource_name):
         """ """
 
 
@@ -161,3 +161,90 @@ class DictMode(Mode):
                     qa_dict[q_tuple] = a_tuple
 
         self.qa_dict = qa_dict
+
+
+@mode
+class TestMode(Mode):
+    """Режим тестирования. Формат данных описан ниже, если не указаны неправильные ответы,
+    они берутся из правильных ответов на другие вопросы"""
+    RESOURCE_PATTERN = 'question = correct_answer_1(ca1) | ca2 | ... ca3 ?' \
+                       ' (optional) incorrect_answer_1(ia1) | ia2 | ... ia3'
+    INCORRECT_ANSWERS_MIN = 2  # Минимальное кол-во неправильных ответов, если они не заданы в файле
+    INCORRECT_ANSWERS_MAX = 5
+    name = 'Тесты'
+    info = 'Выбор правильных ответов на вопрос из предложенного списка (числа через пробел)'
+    settings = {}
+
+    def on_answer(self, answer_text):  # todo check errors
+        ans = []
+        for ind in answer_text.split(' '):
+            ans.append(self._cur_q_list[int(ind.strip()) - 1])
+        self._answers.append(ans)
+        self._counter += 1
+        if self._counter == len(self._questions):
+            self._display_result()
+        else:
+            self._cur_q_list = self._prepare_question()
+            self._display()
+
+    def launch(self):
+        print(self._qa_list)
+        test_length = 5
+        self._questions = random.sample(self._qa_list, test_length)
+        random.shuffle(self._questions)
+        self._counter = 0
+        self._answers = []
+        self._cur_q_list = self._prepare_question()
+        self._display()
+
+    def _parse_resource(self, resource_name):
+        file_path = os.path.join(self.path, resource_name)
+        qa_list = []  # [вопрос, [правильные ответы], [неправильные ответы]]
+        with open(file_path, 'rt', encoding='utf8') as file:
+            for string in file:
+                try:
+                    question, answers = string.strip().split('=')
+                    answers = answers.split('?')
+                    cas = [ans.strip() for ans in answers[0].split('|')]
+                    ias = []
+                    if len(answers) > 1:
+                        ias = [ans.strip() for ans in answers[1].split('|')]
+                    qa_list.append([question.strip(), cas, ias])
+                except IOError:
+                    continue
+        for q in qa_list:  # Если неправильные отверы не указаны, натаскать их из правильных на другие вопросы
+            if not len(q[2]):
+                i = 0
+                limit = random.randint(__class__.INCORRECT_ANSWERS_MIN,
+                                       __class__.INCORRECT_ANSWERS_MAX)
+                while i < limit:
+                    rand_q = random.choice(qa_list)
+                    if rand_q[0] == q[0]:
+                        continue  # not very good
+                    rand_ans = random.choice(rand_q[1])
+                    if rand_ans in q[1]:
+                        continue  # not very good too
+                    q[2].append(rand_ans)
+                    i += 1
+        self._qa_list = qa_list
+
+    def _prepare_question(self):
+        cur_q = self._questions[self._counter]
+        cur_ans = list(cur_q[1]) + list(cur_q[2])
+        random.shuffle(cur_ans)
+        return cur_ans
+
+    def _display(self):
+        cur_q = self._questions[self._counter]
+        d_str = cur_q[0]
+        for i in range(len(self._cur_q_list)):
+            d_str += '\n' + str(i + 1) + '. ' + self._cur_q_list[i]
+        self.display = d_str
+
+    def _display_result(self):
+        res_str = ''
+        for i in range(len(self._questions)):
+            q = self._questions[i]
+            res_str += '\n' + q[0] + ' (' + ', '.join(q[1]) + ') (' +\
+                       ', '.join(self._answers[i]) + ')\n'
+        self._on_exit(res_str)
