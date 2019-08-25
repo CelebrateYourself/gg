ea22b4a8b5bc600bd8ea004e4dccfd074c4347e9
CelebrateYourself <regnainania@mail.ru>
2019-08-25 15:06:54 +0300

    cfg: update package setup settings, add LICENSE

--- setup.py
+++ setup.py
@@ -5,8 +5,12 @@ with open(join(dirname(__file__), 'README.md')) as f:
     description = f.read()
 
 setup(
-    name='gallows',
-    version='0.6.0',
-    packages=find_packages(),
+    name="gallows",
+    version="0.6.0",
+    author="CelebrateYourself",
+    description="The GUI program to test the knowledge with some modes",
     long_description=description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/CelebrateYourself/gg",
+    packages=find_packages(),
 )
